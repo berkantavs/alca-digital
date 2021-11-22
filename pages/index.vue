0c64412a2b82745dd7ec62330b76c60979e3417b
berkantavs <berkantavsar7@gmail.com>
2021-11-22 15:08:46 +0300

    index design fixed

--- pages/index.vue
+++ pages/index.vue
@@ -20,15 +20,15 @@
       <span class="title"> Featured Products </span>
       <div class="products">swiper</div>
     </section>
-    <section class="videos p-5">
+    <section class="videos">
       <b-container fluid>
         <b-row>
-          <b-col md="4">
+          <b-col lg="4" sm="12">
             <span class="videos-title">
               Feel the excellent wet braking with Driveways!
             </span></b-col
           >
-          <b-col md="3" offset-md="5">
+          <b-col lg="3" offset-lg="5" class="d-lg-block d-none">
             <b-button variant="light" block class="text-danger">
               WATCH ALL VIDEOS
             </b-button>
@@ -39,6 +39,23 @@
           <b-col cols="12" lg="9"> </b-col>
           <b-col lg="3" class="d-block d-none"> </b-col>
         </b-row>
+        <div class="video-area">
+          <iframe
+            width="100%"
+            class="iframe"
+            height="193px"
+            src="https://www.youtube.com/embed/JvIRuAW9Maw"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+        <div class="mt-2 d-sm-block d-lg-none">
+          <b-button variant="light" block class="text-danger">
+            WATCH ALL VIDEOS
+          </b-button>
+        </div>
       </b-container>
     </section>
   </div>
